00a628c51fd66a26c8becabb9457e432b5fe8696
mrabramov <abramovdenis888@gmail.com>
2023-07-15 18:13:03 +0300

    add mobile menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,23 +2,35 @@ import classNames from "classnames";
 import { Link } from "react-router-dom";
 import SocialMedia from "../SocialMedia"
 import styles from "./styles.module.css"
+import { FiMenu } from "react-icons/fi";
+import { useState } from "react";
 
 
+function Header() {
+    const [openMibileMenu, setOpenMobileMenu] = useState(false);
 
-function Header(){
     return <header className={styles["header"]}>
         <div className="container">
-        <Link to="/" className={classNames(styles["link"],styles["logo"])}>Logo</Link>
+            <Link to="/" className={classNames(styles["link"], styles["logo"])}>Logo</Link>
 
-        <nav className={styles["menu"]}>
+            <button onClick={() => setOpenMobileMenu(true)} className={classNames(styles['is-mobile'], styles['btn-mobile'], 'btn', 'link')}>
+                <FiMenu />
+            </button>
 
-            <Link to={"/"} className={styles["link"]}>Main</Link>
-            <Link to={"/products"} className={styles["link"]}>Products</Link>
-            <Link to={"/contacts"} className={styles["link"]}>Contacts</Link>
-        </nav>
-        <SocialMedia/>
+            <nav className={classNames(
+                styles["menu"],
+                {
+                    [styles['active']]: openMibileMenu
+                }
+            )}>
+                <Link to={"/"} className={styles["link"]}>Main</Link>
+                <Link to={"/products"} className={styles["link"]}>Products</Link>
+                <Link to={"/contacts"} className={styles["link"]}>Contacts</Link>
+                <button onClick={() => setOpenMobileMenu(false)} className={classNames(styles['btn-close'], 'btn', 'link')}>X</button>
+            </nav>
 
+            <SocialMedia />
         </div>
-    </header>
+    </header >
 }
 export default Header;
